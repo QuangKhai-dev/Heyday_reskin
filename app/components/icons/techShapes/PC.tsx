48be7b79440e7ab80c55b1d73e5387bbe0613ad2
giaomeow <117652451+giaomeow@users.noreply.github.com>
2024-12-25 00:31:17 +0700

    update 25/12

--- app/components/icons/techShapes/PC.tsx
+++ app/components/icons/techShapes/PC.tsx
@@ -1,16 +1,98 @@
 const PC = () => {
   return (
     <svg
-      width='40'
-      height='40'
-      viewBox='0 0 40 40'
-      fill='none'
       xmlns='http://www.w3.org/2000/svg'
+      width='115'
+      height='116'
+      viewBox='0 0 115 116'
+      fill='none'
     >
-      <path
-        d='M4.50167 9.13167L16.7967 7.43833V19.3167H4.5L4.50167 9.13167ZM4.50167 30.8683L16.7967 32.5633V20.83H4.5L4.50167 30.8683ZM18.1483 32.7433L34.5017 35V20.83H18.1483V32.7433ZM18.1483 7.25667V19.3167H34.5017V5L18.1483 7.25667Z'
-        fill='#2a1b04'
-      />
+      <g clip-path='url(#clip0_1_20379)' filter='url(#filter0_f_1_20379)'>
+        <path
+          d='M35.6024 42.7843L52.8154 40.4137V57.0433H35.6001L35.6024 42.7843ZM35.6024 73.2157L52.8154 75.5887V59.162H35.6001L35.6024 73.2157ZM54.7078 75.8407L77.6024 79V59.162H54.7078V75.8407ZM54.7078 40.1593V57.0433H77.6024V37L54.7078 40.1593Z'
+          fill='white'
+        />
+      </g>
+      <g clip-path='url(#clip1_1_20379)' filter='url(#filter1_d_1_20379)'>
+        <path
+          d='M35.6024 42.7843L52.8154 40.4137V57.0433H35.6001L35.6024 42.7843ZM35.6024 73.2157L52.8154 75.5887V59.162H35.6001L35.6024 73.2157ZM54.7078 75.8407L77.6024 79V59.162H54.7078V75.8407ZM54.7078 40.1593V57.0433H77.6024V37L54.7078 40.1593Z'
+          fill='#2B1B04'
+        />
+      </g>
+      <defs>
+        <filter
+          id='filter0_f_1_20379'
+          x='0'
+          y='0'
+          width='116'
+          height='116'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='BackgroundImageFix'
+            result='shape'
+          />
+          <feGaussianBlur
+            stdDeviation='15'
+            result='effect1_foregroundBlur_1_20379'
+          />
+        </filter>
+        <filter
+          id='filter1_d_1_20379'
+          x='20'
+          y='20'
+          width='76'
+          height='76'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+            result='hardAlpha'
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation='5' />
+          <feComposite in2='hardAlpha' operator='out' />
+          <feColorMatrix
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+          />
+          <feBlend
+            mode='normal'
+            in2='BackgroundImageFix'
+            result='effect1_dropShadow_1_20379'
+          />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='effect1_dropShadow_1_20379'
+            result='shape'
+          />
+        </filter>
+        <clipPath id='clip0_1_20379'>
+          <rect
+            width='56'
+            height='56'
+            fill='white'
+            transform='translate(30 30)'
+          />
+        </clipPath>
+        <clipPath id='clip1_1_20379'>
+          <rect
+            width='56'
+            height='56'
+            fill='white'
+            transform='translate(30 30)'
+          />
+        </clipPath>
+      </defs>
     </svg>
   )
 }
