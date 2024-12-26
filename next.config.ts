57689e491a77a07582aff07151a5d90e5aeaa887
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-12-26 22:15:48 +0700

    disable optimized image

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|webp|woff2?)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable' // Cache dài hạn
-          }
-        ]
-      }
-    ]
-  },
   /* config options here */
   webpack: (config, {}) => {
     config.module.generator.asset.publicPath = '/_next/'
 
     return config
+  },
+  images: {
+    unoptimized: true
   }
 }
 
